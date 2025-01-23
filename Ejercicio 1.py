19b19125ecc70916764c2de7502c24a7ad6fd0d6
Olopper <olopper2407@g.educaand.es>
2025-01-23 09:56:23 +0100

    Exercise 1, 4

---
+++ Ejercicio 1.py
@@ -0,0 +1,5 @@
+edad = int(input("Introduzca su edad: "))
+if edad > 18:
+    print("Puedes votar")
+else:
+    print("No puedes votar")
---
+++ Ejercicio 6.py
@@ -0,0 +1,9 @@
+def check_chars():
+    word = input("Introduce la palabra:")
+    for char in word:
+        if "@" == char:
+            print("La palabra tiene el caracter @")
+        if "#" == char:
+            print("La palabra tiene el caracter #")
+
+check_chars
