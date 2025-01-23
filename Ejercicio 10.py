c1151d1b16b4742cd07c411d0d3752070f274d31
unknown <olopper@g.educaand.es>
2025-01-23 18:50:52 +0100

    Final exercise

---
+++ Ejercicio 10.py
@@ -0,0 +1,9 @@
+def estrellas():
+    estrellas_cantidad = int(input("Ingrese el número de estrellas que desee imprimir: "))
+    for i in range(1 ,estrellas_cantidad +1):
+        print("★"*i)
+        
+    return None
+
+estrellas()
+    
